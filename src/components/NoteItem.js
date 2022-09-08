3cc7547b2b811f50d38ade784a9705c818a2e67f
HendP <hendpermana12@gmail.com>
2022-09-09 00:35:17 +0700

    fixing date not display

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import DeleteButton from './DeleteButton';
+import { showFormattedDate } from '../utils';
 
-function NoteItem({ title, body, createAt, id, onDelete }) {
+function NoteItem({ title, body, createdAt, id, onDelete }) {
     return (
         <div className="note-item">
             <div className="note-item__content">
                 <h3 className="note-item__title">{title}</h3>
-                <p className="note-item__date">{createAt}</p>
+                <p className="note-item__date">{showFormattedDate(createdAt)}</p>
                 <p className="note-item__body">{body}</p>
             </div>
             <div className="note-item__action">
